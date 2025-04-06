51b95b93364486cad5dfbeafc0d6ba0b08303bd5
Sakthisudlai <161962378+sakthisakthi9684@users.noreply.github.com>
2025-04-06 22:35:19 +0530

    DEPLOYEE

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,18 +21,15 @@ const Navbar = ({ userId, setUserId }) => {
   return (
     <nav className="bg-[#1E293B] text-white p-4 flex justify-between items-center shadow-md fixed w-full top-0 z-10">
       <h1 className="text-2xl font-bold bg-gradient-to-r from-indigo-500 to-teal-400 text-transparent bg-clip-text mb-0 text-nowrap">
-        {navbarTitle}{" "}
-        <span className="font-bold text-5xl bg-gradient-to-r from-fuchsia-500 via-orange-400 via-amber-500 via-green-400 via-blue-500 to-purple-500 text-transparent bg-clip-text ml-3">
-          */*{" "}
-        </span>
-        CHAT-APP
+        {navbarTitle}
       </h1>
 
       <div className="flex space-x-4">
-      
         {userId ? (
           <>
-            <Link to="/chat" className="hover:text-blue-400">Chat</Link>
+            <Link to="/chat" className="hover:text-blue-400">
+              Chat
+            </Link>
             <button
               onClick={handleLogout}
               className="bg-red-500 px-3 py-1 rounded-lg hover:bg-red-600"
@@ -42,7 +39,9 @@ const Navbar = ({ userId, setUserId }) => {
           </>
         ) : (
           <>
-             <Link to="/" className="hover:text-blue-400">Home</Link> 
+            <Link to="/" className="hover:text-blue-400">
+              Home
+            </Link>
           </>
         )}
       </div>
