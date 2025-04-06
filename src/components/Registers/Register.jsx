db3fc332508be8cb0ef2299802773145d5d78991
Sakthisudlai <161962378+sakthisakthi9684@users.noreply.github.com>
2025-04-06 23:01:06 +0530

    Update Register.jsx

--- src/components/Registers/Register.jsx
+++ src/components/Registers/Register.jsx
@@ -1,24 +1,23 @@
-  import React, { useState } from "react";
-  import EmailVerification from "../Registers/EmailVerification.jsx";
-  import OtpVerification from "../Registers/OtpVerification.jsx";
-  import RegisterForm from "../Registers/RegisterForm.jsx";
+import React, { useState } from "react";
+import EmailVerification from "../Registers/EmailVerification.jsx";
+import OtpVerification from "../Registers/OtpVerification.jsx";
+import RegisterForm from "../Registers/RegisterForm.jsx";
 
+const Register = () => {
+  const [step, setStep] = useState(1);
+  const [email, setEmail] = useState("");
 
-  const Register = () => {
-    const [step, setStep] = useState(1);
-    const [email, setEmail] = useState("");
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
+      {step === 1 && (
+        <EmailVerification setEmail={setEmail} goToOtp={() => setStep(2)} />
+      )}
+      {step === 2 && (
+        <OtpVerification email={email} goToRegister={() => setStep(3)} />
+      )}
+      {step === 3 && <RegisterForm email={email} />}
+    </div>
+  );
+};
 
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
-        {step === 1 && (
-          <EmailVerification setEmail={setEmail} goToOtp={() => setStep(2)} />
-        )}
-        {step === 2 && (
-          <OtpVerification email={email} goToRegister={() => setStep(3)} />
-        )}
-        {step === 3 && <RegisterForm email={email} />}
-      </div>
-    );
-  };
-
-  export default Register;
+export default Register;
